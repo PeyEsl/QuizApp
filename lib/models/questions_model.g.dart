948a43273a444716fcaafcd766ee5a52ac3585fb
PeyEsl <eslami_peyman@ymail.com>
2024-04-17 03:33:05 +0330

    14030128

---
+++ lib/models/questions_model.g.dart
@@ -0,0 +1,53 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'questions_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class QuestionAdapter extends TypeAdapter<Question> {
+  @override
+  final int typeId = 1;
+
+  @override
+  Question read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Question(
+      fields[0] as int,
+      fields[1] as String,
+      fields[2] as String,
+      fields[3] as String,
+      fields[4] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Question obj) {
+    writer
+      ..writeByte(5)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.question)
+      ..writeByte(2)
+      ..write(obj.firstAnswer)
+      ..writeByte(3)
+      ..write(obj.secondAnswer)
+      ..writeByte(4)
+      ..write(obj.trueAnswer);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is QuestionAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
